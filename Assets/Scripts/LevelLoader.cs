83dade98313e0d88c992cadae0e4346f4f26fc5e
Christian <leec22@rpi.edu>
2018-09-08 18:24:15 -0400

    Added level file and edited LevelLoader

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LevelLoader : MonoBehaviour {
+
+    public List<List<bool>> pacMov = new List<List<bool>>();
+
+    // Use this for initialization
+    void Start () {
+        ReadFile("tileset.txt");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReadFile(string path) {
+        StreamReader reader = new StreamReader(path);
+        List<bool> tempList = new List<bool>();
+        pacMov.Add(tempList);
+
+        while (!reader.EndOfStream) {
+            if (reader.Peek() == '\n') {
+                tempList = new List<bool>();
+            } else if (reader.Peek() == ' ') {
+                tempList.Add(true);
+            } else if (reader.Peek() == '#') {
+                tempList.Add(false);
+            }
+            reader.Read();
+        }
+        reader.Close();
+        reader.Dispose();
+    }
+}
